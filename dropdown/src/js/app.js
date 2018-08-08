10920c45c74a11996defe83777cd047464f354b6
Dongwoo Lee <likedemian@me.com>
2018-08-08 18:28:23 +0900

    Fix: Drop Down Features

--- dropdown/src/js/app.js
+++ dropdown/src/js/app.js
@@ -2,14 +2,17 @@ var dropBtn = document.querySelector('.header__drop-button');
 var menuList = document.querySelectorAll('.header__items');
 
 
-dropBtn.addEventListener('click', event => {
+dropBtn.addEventListener('mouseover', event => {
   menuList.forEach(el => {
-    if (el.classList.contains('hide')) {
-      el.classList.remove('hide');
-      el.classList.add('show');
-    } else {
-      el.classList.remove('show');
-      el.classList.add('hide');
-    }
+    el.classList.remove('hide');
+    el.classList.add('show');
+  });
+})
+
+
+dropBtn.addEventListener('mouseleave', event => {
+  menuList.forEach(el => {
+    el.classList.remove('show');
+    el.classList.add('hide');
   });
 })
